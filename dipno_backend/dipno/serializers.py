4037ef8a3ada84baff10ff9044a31ecdcb0668b9
Jimbo Freedman <jimbo@jimbo.rocks>
2020-05-10 11:56:04 -0600

    JSON API

--- dipno_backend/dipno/serializers.py
+++ dipno_backend/dipno/serializers.py
@@ -1,8 +1,8 @@
-from rest_framework import serializers
+from rest_framework_json_api import serializers
 from ..users.models import User
 
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(read_only=True)
     last_name = serializers.CharField(read_only=True)
